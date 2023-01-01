13c1bb86ece0f6add1a295583eaa9a4fd0679b1a
Kelechi Obioha <kelechi.obioha@mykenzie.snhu.edu>
2023-01-01 11:14:29 -0600

    Completed, tested and passed both getAllListngs and getAllOpenListings integrationTests

--- IntegrationTests/src/test/java/com/kenzie/appserver/controller/ListingControllerTest.java
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/ListingControllerTest.java
@@ -8,13 +8,9 @@ import com.kenzie.appserver.IntegrationTest;
 import com.kenzie.appserver.config.ListingGenerator;
 import com.kenzie.appserver.controller.model.ListingCreateRequest;
 import com.kenzie.appserver.controller.model.ListingResponse;
-import com.kenzie.appserver.repositories.model.ListingRecord;
 import com.kenzie.appserver.service.ListingService;
 import com.kenzie.appserver.service.model.Listing;
-import net.andreinc.mockneat.MockNeat;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,10 +18,10 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -34,41 +30,56 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @IntegrationTest
-public class ListingControllerTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ListingControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @Autowired
     ListingService listingService;
+    private ObjectMapper mapper;
+    private ListingGenerator listingGenerator;
+    private List<Listing> newListings;
 
-    private final MockNeat mockNeat = MockNeat.threadLocal();
+    @BeforeAll
+    public void setup() {
+        mapper = new ObjectMapper();
+        listingGenerator = new ListingGenerator();
 
-    private final ObjectMapper mapper = new ObjectMapper();
-    private final ListingGenerator listingGenerator = new ListingGenerator();
+        newListings = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            newListings.add(listingGenerator.generateListing());
+        }
 
+        for (Listing listing : newListings) {
+            listingService.createNewListing(listing);
+        }
+    }
+
+    @AfterAll
+    public void cleanUp() {
+        System.out.println("After All cleanUp() method called");
+        for (Listing listing : newListings) {
+            listingService.deleteListing(listing.getListingNumber());
+        }
+    }
 
     @Test
     public void getAllListings_successful() throws Exception {
         // GIVEN
-        List<Listing> listings = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            listings.add(listingGenerator.generateListing());
-        }
-
-        for (Listing listing : listings) {
-            listingService.createNewListing(listing);
-        }
+        // Already created newListings in setup()
 
         // WHEN
-        MvcResult result =  mvc.perform(get("/listing")
+        MvcResult allListingsResult =  mvc.perform(get("/listing")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()).andReturn();
 
         // THEN
-        String response = result.getResponse().getContentAsString();
-        assertThat(response).isNotNull();
-        Assertions.assertTrue(response.contains("listingNumber"));
-
+        String response = allListingsResult.getResponse().getContentAsString();
+        List<ListingResponse> allListingsResponseList = mapper.readValue(response, new TypeReference<List<ListingResponse>>() {});
+        assertThat(allListingsResponseList).isNotEmpty()
+                .hasSizeGreaterThan(0);
+        assertEquals(newListings.size(), allListingsResponseList.size());
     }
 
     @Test
@@ -127,4 +138,22 @@ public class ListingControllerTest {
         assertThat(listingService.findByListingNumber(listing.getListingNumber())).isNull();
 
     }
+
+    @Test
+    public void getAllOpenListings_successful() throws Exception {
+        // GIVEN
+        // Already created newListings in setup()
+
+        // WHEN
+        MvcResult openListingsResult =  mvc.perform(get("/listing/allOpen")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andReturn();
+
+        // THEN
+        String response = openListingsResult.getResponse().getContentAsString();
+        List<ListingResponse> openListingsResponseList = mapper.readValue(response, new TypeReference<List<ListingResponse>>() {});
+        assertThat(openListingsResponseList).isNotEmpty().hasSizeGreaterThan(0);
+        assertThat(openListingsResponseList.get(0).getListingStatus()).isEqualToIgnoringCase("For Sale");
+    }
+
 }
