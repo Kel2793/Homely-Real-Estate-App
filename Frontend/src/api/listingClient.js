fb3fed2d399d76c58116f4eec884e28ed4f44dee
Kelechi Obioha <kelechi.obioha@mykenzie.snhu.edu>
2023-01-07 19:22:56 -0600

    search criteria result display revision 1

---
+++ Frontend/src/api/listingClient.js
@@ -0,0 +1,79 @@
+import BaseClass from "../util/baseClass";
+import axios from 'axios'
+
+/**
+ * Client to call the ListingService.
+ */
+export default class ListingClient extends BaseClass {
+
+    constructor(props = {}){
+        super();
+        const methodsToBind = ['clientLoaded', 'getParameterizedListings','getAllOpenListings', 'createListing' ];
+        this.bindClassMethods(methodsToBind, this);
+        this.props = props;
+        this.clientLoaded(axios);
+    }
+
+    /**
+     * Run any functions that are supposed to be called once the client has loaded successfully.
+     * @param client The client that has been successfully loaded.
+     */
+    clientLoaded(client) {
+        this.client = client;
+        if (this.props.hasOwnProperty("onReady")){
+            this.props.onReady();
+        }
+    }
+
+    async createListing(address, price, numBedrooms, numBathrooms, squareFootage, listingStatus, lotSize, errorCallback) {
+        try {
+            const response = await this.client.post(`/listing`, {
+                "address": address,
+                "squareFootage": squareFootage,
+                "price": price,
+                "numBedrooms": numBedrooms,
+                "numBathrooms": numBathrooms,
+                "listingStatus": listingStatus,
+                "lotSize": lotSize
+            });
+            return response.data;
+        } catch (error) {
+            this.handleError("createListing", error, errorCallback);
+        }
+    }
+
+
+    async getParameterizedListings(squareFootage, price, numBedrooms, numBathrooms, lotSize, errorCallback) {
+
+        try {
+            const response = await this.client.get(`/listing/query?squareFootage=${squareFootage}&price=${price}&numBedrooms=${numBedrooms}&numBathrooms=${numBathrooms}&lotSize=${lotSize}`);
+            return response.data;
+        } catch (error) {
+            this.handleError("getParameterizedListings", error, errorCallback)
+        }
+    }
+
+    async getAllOpenListings(errorCallback) {
+        try {
+            const response = await this.client.get(`/listing/allOpen`);
+            return response.data;
+        } catch (error) {
+            this.handleError("getAllOpenListings", error, errorCallback)
+        }
+    }
+
+    /**
+     * Helper method to log the error and run any error functions.
+     * @param error The error received from the server.
+     * @param errorCallback (Optional) A function to execute if the call fails.
+     */
+    handleError(method, error, errorCallback) {
+        console.error(method + " failed - " + error);
+        if (error.response.data.message !== undefined) {
+            console.error(error.response.data.message);
+        }
+        if (errorCallback) {
+            errorCallback(method + " failed - " + error);
+        }
+    }
+}
--- Frontend/src/pages/homePage.js
+++ Frontend/src/pages/homePage.js
@@ -1,6 +1,6 @@
 import BaseClass from "../util/baseClass";
 import DataStore from "../util/DataStore";
-import ExampleClient from "../api/exampleClient";
+import ListingClient from "../api/listingClient";
 
 /**
  * Logic needed for the view playlist page of the website.
@@ -9,7 +9,7 @@ class HomePage extends BaseClass {
 
     constructor() {
         super();
-        this.bindClassMethods(['onGet', 'onCreate', 'renderHome'], this);
+        this.bindClassMethods(['onSearch', 'onGetListings', 'onCreate', 'renderHomeListing', 'renderHomeSearch'], this);
         this.dataStore = new DataStore();
     }
 
@@ -17,63 +17,158 @@ class HomePage extends BaseClass {
      * Once the page has loaded, set up the event handlers and fetch the concert list.
      */
     async mount() {
-        document.getElementById('get-by-id-form').addEventListener('submit', this.onGet);
         document.getElementById('create-form').addEventListener('submit', this.onCreate);
-        this.client = new ExampleClient();
+        document.getElementById('search-homes-form').addEventListener('submit', this.onSearch);
+        this.client = new ListingClient();
 
-        this.dataStore.addChangeListener(this.renderExample)
+        this.dataStore.addChangeListener(this.renderHomeSearch);
+        this.dataStore.addChangeListener(this.renderHomeListing);
+        this.onGetListings();
+        this.onSearch();
     }
 
     // Render Methods --------------------------------------------------------------------------------------------------
 
-    async renderHome() {
+    async renderHomeListing() {
+        let listingResultArea = document.getElementById("newListing-info");
+
+        const newListing = this.dataStore.get("newHomes");
+
+        if (newListing) {
+            listingResultArea.innerHTML = `
+                <div class="row">
+                    <table class="table-bordered">
+                        <thead>
+                            <tr>
+                                <th> Listing Number</th>
+                                <th> Address</th>
+                                <th> Square Footage</th>
+                                <th> Price</th>
+                                <th> Number of Bedrooms</th>
+                                <th> Number of Bathrooms</th>
+                                <th> Listing Status</th>
+                                <th> Lot Size</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+            `;
+
+                           // for (let newListing of newListings) {
+                                listingResultArea.innerHTML += `
+                                 <tr>
+                                    <td> ${newListing.listingNumber}</td>
+                                    <td> ${newListing.address} </td>
+                                    <td> ${newListing.squareFootage} </td>
+                                    <td> ${newListing.price} </td>
+                                    <td> ${newListing.numBedrooms} </td>
+                                    <td> ${newListing.numBathrooms} </td>
+                                    <td> ${newListing.listingStatus} </td>
+                                    <td> ${newListing.lotSize} </td>
+                                </tr>`;
+                           // }
+            listingResultArea.innerHTML += `
+                        </tbody>
+                    </table>`;
+
+        } else {
+            listingResultArea.innerHTML = "No Item";
+        }
+    }
+
+
+    async renderHomeSearch() {
         let resultArea = document.getElementById("result-info");
+        var searchResults = [];
 
-        const example = this.dataStore.get("example");
+        searchResults = this.dataStore.get("searchedHomes");
 
-        if (example) {
+        if (searchResults) {
             resultArea.innerHTML = `
-                <div>ID: ${example.id}</div>
-                <div>Name: ${example.name}</div>
-            `
+                <div class="row">
+                    <table class="table-bordered">
+                        <thead>
+                            <tr>
+                                <th> Listing Number</th>
+                                <th> Address</th>
+                                <th> Square Footage</th>
+                                <th> Price</th>
+                                <th> Number of Bedrooms</th>
+                                <th> Number of Bathrooms</th>
+                                <th> Listing Status</th>
+                                <th> Lot Size</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+            `;
+                         for (let searchResult of searchResults) {
+                            resultArea.innerHTML += `
+                                 <tr>
+                                    <td> ${searchResult.listingNumber}</td>
+                                    <td> ${searchResult.address} </td>
+                                    <td> ${searchResult.squareFootage} </td>
+                                    <td> ${searchResult.price} </td>
+                                    <td> ${searchResult.numBedrooms} </td>
+                                    <td> ${searchResult.numBathrooms} </td>
+                                    <td> ${searchResult.listingStatus} </td>
+                                    <td> ${searchResult.lotSize} </td>
+                                </tr>`;
+                            }
+            resultArea.innerHTML += `
+                         </tbody>
+                    </table>`;
         } else {
             resultArea.innerHTML = "No Item";
         }
     }
 
+
     // Event Handlers --------------------------------------------------------------------------------------------------
 
-    async onGet(event) {
-        // Prevent the page from refreshing on form submit
-        event.preventDefault();
+    async onGetListings() {
+        let result = await this.client.getAllOpenListings(this.errorHandler);
+        this.dataStore.set("listing", result);
+    }
 
-        let id = document.getElementById("id-field").value;
-        this.dataStore.set("home", null);
+    async onSearch() {
 
-        let result = await this.client.getExample(id, this.errorHandler);
-        this.dataStore.set("home", result);
-        if (result) {
-            this.showMessage(`Got ${result.name}!`)
+        let squareFootage = document.getElementById("min-squareFootage").value;
+        let price = document.getElementById("max-price").value;
+        let numBedrooms = document.getElementById("min-bedroom").value;
+        let numBathrooms = document.getElementById("min-bathroom").value;
+        let lotSize = document.getElementById("min-lotSize").value;
+
+        const searchedHomes = await this.client.getParameterizedListings(squareFootage, price, numBedrooms, numBathrooms, lotSize);
+
+        this.dataStore.set("searchedHomes", searchedHomes);
+
+        if (searchedHomes) {
+            this.showMessage(`Search successful!`)
         } else {
-            this.errorHandler("Error doing GET!  Try again...");
+            this.errorHandler("Error searching!  Try again...");
         }
     }
 
     async onCreate(event) {
         // Prevent the page from refreshing on form submit
         event.preventDefault();
-        this.dataStore.set("home", null);
 
-        let name = document.getElementById("create-name-field").value;
+        let address = document.getElementById("address").value;
+        let price = document.getElementById("price").value;
+        let numBedrooms = document.getElementById("bedrooms").value;
+        let numBathrooms = document.getElementById("bathrooms").value;
+        let squareFootage = document.getElementById("squareFootage").value;
+        let listingStatus = document.getElementById("listingStatus").value;
+        let lotSize = document.getElementById("lotSize").value;
 
-        const createdExample = await this.client.createExample(name, this.errorHandler);
-        this.dataStore.set("home", createdExample);
+        const createdListing = await this.client.createListing(address, price, numBedrooms, numBathrooms, squareFootage, listingStatus, lotSize, this.errorHandler);
+        this.dataStore.set("newHomes", createdListing);
 
-        if (createdExample) {
-            this.showMessage(`Created ${createdExample.name}!`)
+        if (createdListing) {
+            this.showMessage(`Created Listing!`)
         } else {
             this.errorHandler("Error creating!  Try again...");
         }
+        this.onGetListings();
     }
 }
 
